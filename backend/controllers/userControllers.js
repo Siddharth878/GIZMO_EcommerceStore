ab9751b1a3c09fe3ab052bdd9bad92e9abcd06ad
Siddharth Saxena <79091726+Siddharth878@users.noreply.github.com>
2024-07-17 17:10:21 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -14,9 +14,7 @@ const createSendToken = (user,statusCode,res)=>{
     
 
     const options = {
-        // expires : new Date(
-        //     Date.now() + process.env.COOKIE_EXPIRE *24*60*60*1000
-        // ),
+        expires:"2d"
         httpOnly:true
     };
 
